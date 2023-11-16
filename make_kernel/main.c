51ac2362eddbcb4f9c2debfe4fe503569e8a3417
Martin Simon <martis61@andorra.imp.fu-berlin.de>
2023-11-16 13:57:25 +0100

    isr working on

--- make_kernel/main.c
+++ make_kernel/main.c
@@ -1,12 +1,12 @@
-#include "../debug_unit/debug_unit.h"
-#include "../time/time.h"
+#include "../debug_unit/debug_unit.c"
+//#include "../time/time.c"
+#include "../ISR/isr.c"
 
 int main() {
    activateDBGU();
-   
+   isr_reset();
    char msg[] = "MoinsenOS";
    printf("String: %s, Char: %c, Hex: %x, Pointer: %p, Number Positive: %d, Number Positive: %d, end", "String", 'C', 0xABCD1234, msg, 550, -3424);
-   
    printf("END");
 
    return 0;
--- make_kernel/main.d
+++ make_kernel/main.d
@@ -1,5 +1,10 @@
-main.o: main.c ../debug_unit/debug_unit.h ../time/time.h
+main.o: main.c ../debug_unit/debug_unit.c ../debug_unit/debug_unit.h \
+ ../ISR/isr.c ../ISR/isr.h
+
+../debug_unit/debug_unit.c:
 
 ../debug_unit/debug_unit.h:
 
-../time/time.h:
+../ISR/isr.c:
+
+../ISR/isr.h:
